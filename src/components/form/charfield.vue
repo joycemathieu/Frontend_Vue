f36fd18bf5e2494ba55b2157fc0f8b00dfcf3bdb
YoannN2 <yoann_nouveau@hotmail.com>
2021-05-11 13:16:52 +1100

    pre-merge save

---
+++ src/components/form/charfield.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- {{ }} -->
+    <div class="field is-horizontal">
+      <div class="field-label">
+        <label :for="name" class="label">{{label}}</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input" type="text" :name="name" v-model="value" v-on:change="sendValue">
+          </div>
+        </div>
+      </div>
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'charfield',
+  props: {
+    label: String,
+    name: String,
+  },
+  data: () => {
+    return {
+      value: "",
+    }
+  },
+  methods: {
+    sendValue: function () {
+      this.$emit("value_change", {value: this.value, name: this.name})
+    }
+  }
+}
+</script>
---
+++ src/components/signup_user.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="SignupUser">
+    <h1>Login</h1>
+
+    <form v-on:submit.prevent="submitForm">
+
+      <label for="username">Identifiant</label>
+      <input type="text" name="username" v-model="username">
+
+      <label for="password">Mot de Passe</label>
+      <input type="password" name="password" v-model="password">
+
+      <button type="submit" v-on:click="submitForm">S'identifier</button>
+      <p>Pas inscrit? <router-link to="/signup">Inscrivez-vous</router-link></p>
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignupUser',
+  props: {
+  },
+  data () {
+    return {
+      username: "",
+      password: "",
+    }
+  },
+  methods : {
+    submitForm: function () {
+      alert("submitForm: " + this.username + " " + this.password)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
