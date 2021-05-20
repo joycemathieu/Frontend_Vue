4d17f4f94639e993912d7c0dc714111ff34c6882
Jeremy Oger <j.oger@cci-formation.nc>
2021-05-20 13:12:44 +1100

    init calendar

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>
-      
+    <div>
+        <FormulateForm v-model="values">
+            <FormulateInput 
+                type="date"
+                label="Periode du"
+                name="debut"
+            />
+            <FormulateInput 
+                type="date"
+                label="Au"
+                name="fin"
+            />
+           
+            <FormulateInput
+                type="group"
+                name="horraires"
+                :repeatable=true
+                add-label="Ajouter une horraire"
+                @repeatableAdded="test"
+            >
+                <h4> {{ setLabel }} </h4>
 
-    
-  </div>
+                <FormulateInput 
+                    type="time"
+                    label="de"
+                    name="de"
+                />
+                <FormulateInput 
+                    type="time"
+                    name="a"
+                    label="a"
+                />
+
+            </FormulateInput>
+
+            <FormulateInput 
+                type="submit"
+                name="save"
+            />
+
+        </FormulateForm>
+        <md-content >
+            <md-tab>
+                <h3>Previsualisation des valeurs aux submits</h3>
+                <pre v-text="values" > </pre>
+            </md-tab>
+        </md-content>
+    </div>
 </template>
 
 <script>
@@ -14,12 +57,26 @@ export default {
         return{
             values:{},
             userSaved:false,
-
+            days: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            months: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre']
         }
     },
     methods:{
-        submit(){
-            this.userSaved = true;
+        test(){
+            console.log("yolo je me répéte !")
+            
+        }
+    },
+    computed: {
+        setLabel () {
+            let nbJour = new Date(this.values.debut).getDate();
+            let jour = new Date(this.values.debut).getDay();
+            let mois = new Date(this.values.debut).getMonth();
+
+            if(isNaN(nbJour) == false && isNaN(jour) == false && isNaN(mois) == false){
+                return "horraire pour le " + this.days[jour] + " " + nbJour + " " + this.months[mois] 
+            }
+            return ''
         }
     }
 
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,65 +1,24 @@
 <template>
     <div>
-        <FormulateForm v-model="values">
-            <FormulateInput 
-                type="group"
-                name="tester"
-                :repeatable=true
-                add-label = "add new day"
-                #default="{ index }"
-            >
-                <p> Look my IndexGroup {{ index }} </p>
-                <FormulateInput 
-                    type="date"
-                    label="Periode du"
-                    name="debut"
-                    :value="index"
-                />
-                <FormulateInput 
-                    type="date"
-                    label="Au"
-                    name="fin"
-                />
-                <FormulateInput 
-                    type="group"
-                    name="horraire"
-                    label="Horraire"
-                    v-for="(day,index) in days"
-                    :key="index"
-                    
-                >
-                    <FormulateInput 
-                        type="time"
-                        label="de"
-                        name="de"
-                    />
-                    <FormulateInput 
-                        type="time"
-                        label="a"
-                        name="a"
-                    />
-                </FormulateInput>
-            </FormulateInput>
-        </FormulateForm>
+        
+            <Calendrier />
+        
 
-        <md-content >
-            <md-tab>
-                <h3>Previsualisation des valeurs aux submits</h3>
-                <pre v-text="values" > </pre>
-            </md-tab>
-        </md-content>
+
     </div>
 </template>
 
 <script>
+import Calendrier from "../components/calendar"
 export default {
     name:"showCalendar",
+    components:{
+        Calendrier
+    },
     data(){
         return{
             values:{},
-            test:"",
-            debut:null,
-            fin:null
+
         }
     },
     methods:{
@@ -67,31 +26,6 @@ export default {
             console.log("je suis dans le group !")
         }
     },
-    computed: {
-        days () {
-            console.log("tester",this.values.tester)
-            //Probleme de ouff !!! OoO !!! 
-            for(let i =0; this.values.tester ;i++){
-                console.log("item",i);
-           
-                let startFormat = new Date(this.values.tester[i].debut).getTime() / 86400000;
-                let endFormat = new Date(this.values.tester[i].fin).getTime() / 86400000;
-                if (this.debut !=0 && this.fin != 0) {
-                    var output = []
-                    const n_days = Math.floor((endFormat - startFormat)) + 1;
-                    for (let i = 0; i < n_days; i++){
-                        output.push(this.debut + i * 86400000);
-                    
-                    }
-                    return output
-                }
-                else {
-                    return this.debut ? this.debut : Date()
-                }
-             }
-             return 1
-        }
-    }
 }
 </script>
 
